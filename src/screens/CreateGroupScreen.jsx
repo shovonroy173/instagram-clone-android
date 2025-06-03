0a251eedc5ff99f1d16c455b5598ff2503b115c3
Shovon ROY <shovonroy2003@gmail.com>
2025-06-03 06:21:07 +0600

    mod: formate date update

---
+++ src/screens/CreateGroupScreen.jsx
@@ -0,0 +1,12 @@
+import { View, Text } from 'react-native'
+import React from 'react'
+
+const CreateGroupScreen = () => {
+  return (
+    <View>
+      <Text>CreateGroup</Text>
+    </View>
+  )
+}
+
+export default CreateGroupScreen
